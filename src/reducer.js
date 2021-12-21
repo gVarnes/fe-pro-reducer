52a78ccbb64d4af67be2deab6dd08574f40316ea
Vladislav Kolomoiets <vellenger17@gmail.com>
2021-12-21 21:01:30 +0200

    added a code to reducer.js file

--- src/reducer.js
+++ src/reducer.js
@@ -1,5 +1,20 @@
 function reducer(input, action) {
   // Ваше решение тут
+  switch (action) {
+    case 'upperCase':
+      return string.toUpperCase();
+
+
+    case 'lowerCase':
+      return string.toLowerCase();
+    case 'capitalize':
+      const arrayWithWords = string.split(' ');
+      return arrayWithWords.map(item => `${item[0].toUpperCase()}${item.slice(1)}`).join(' ');
+    // return string.split(' ').map(item => `${item[0].toUpperCase()}${item.slice(1)}`).join(' ');
+    // return capitalize(string);
+    default:
+      return string;
+  }
 }
 
 module.exports = reducer;
